198636704b6f3fbc1bcaffb76d1deaf9eb61511c
Johnathon Wood <jaywood128@gmail.com>
2019-07-27 13:38:17 -0400

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -20,6 +20,14 @@ class Pancake extends React.Component {
     });
   };
 
+  componentDidMount = () => {
+    this.startInterval()
+  }
+
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
+
   startInterval = () => {
     this.interval = setInterval(this.updateCounter, 1000);
   };
